d7850f3d1fa69da36eca3d942dfb59e354acfdc7
Cory Champney <thechamp@champ.local>
2023-06-09 14:21:22 -0500

    configuring client to send proof to server and server to verify proof

--- client/index.js
+++ client/index.js
@@ -6,9 +6,17 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const name = "Beth Stracke";
+  const index = niceList.findIndex(n => n === name);
+  const mTree = new MerkleTree(niceList);
+  const proof = mTree.getProof(index);
+
+
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: JSON.stringify(proof),
+    name: name
   });
 
   console.log({ gift });
